66c204f6e9f8869dbffe2b912ca1936e45548ecb
mohamedgaberyoussef1996 <mohamedgaberyoussef1996@gmail.com>
2023-05-25 21:29:20 +0300

    add new freatures

---
+++ lib/features/domain/usecases/get_authentication_usecase.dart
@@ -0,0 +1,16 @@
+
+
+import '../../../core/resources/export_file.dart';
+import 'base_usecase.dart';
+
+class GetAuthenticationUseCase implements BaseUseCase<Params, AppAuthenticationLevel> {
+  final AppRepository _repository;
+
+  GetAuthenticationUseCase({required AppRepository repository}):_repository=repository;
+
+  @override
+  Future<Either<Failure, AppAuthenticationLevel>> execute(Params params)  {
+    return  _repository.getLevelAuthenticationApp();
+  }
+}
+
